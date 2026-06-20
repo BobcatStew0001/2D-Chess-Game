24cb600eba68970c5b63db4960b48bba0e5722df
Zach Brown <bigcountry66789@gmail.com>
2026-06-20 09:36:17 -0500

    Completed the AI Controller

--- Chess_Backend/Controllers/AIController.cs
+++ Chess_Backend/Controllers/AIController.cs
@@ -1,6 +1,31 @@
-﻿namespace Chess_Backend.Controllers;
+﻿using Chess_Backend.Logic;
+using Chess_Backend.Models;
+using Microsoft.AspNetCore.Mvc;
 
-public class AIController
+namespace Chess_Backend.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+
+public class AIController:ControllerBase
 {
+    private readonly ChessAI _ai;
+    private readonly GameState _gameState;
+    
+    public AIController(GameState gameState, ChessAI ai)
+    {
+        _gameState = gameState;
+        _ai = ai;
+      
+        
+    }
+[HttpPost("aimove")]
+public IActionResult AIMove()
+{
+    var bestMove = _ai.GetBestMove(_gameState.Board, PieceColor.Black);
+    _gameState.MakeMove(bestMove);
+    return Ok(bestMove);
+
+
+}
     
 }
